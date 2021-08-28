9ce9d0d449bc574ea7d9121093fa64d0e6f94150
luosq0123 <835235763@qq.com>
2021-08-29 01:57:30 +0800

    11.2 Camera Viewing Geometry，完善相机类

--- camera.h
+++ camera.h
@@ -9,22 +9,30 @@
 
 class Camera {
 public:
-    Camera(double vfov,     // vertical field-of-view in degrees
+    // 这几个参数是世界坐标下的
+    Camera(Point3d lookfrom,    // 相机的位置
+           Point3d lookat,      // 相机看向的点
+           Vec3d viewup,        // 相机的上方，按理来说应该与在 与看向方向垂直的那个平面 上，但这里的 viewup 没有要求那么高，只要能投影到那个平面上就行
+           double vfov,         // vertical field-of-view in degrees
            double aspect_ratio) {
         double theta = degreesToRadians(vfov);
-        double h = tan(theta / 2);
-        double viewport_height = 2.0 * h;
-        double viewport_width = aspect_ratio * viewport_height;
+        double h = tan(theta / 2);      // 这里应该有个隐含条件是 focal_length == 1.0，h = focal_length * tan(theta / 2)
+        double viewport_height = 2.0 * h;   // 世界坐标下的 height 长度
+        double viewport_width = aspect_ratio * viewport_height;     // 世界坐标下的 width 长度
 
-        double focal_length = 1.0;
+        // w, u, v 是相机的正交基
+        Vec3d w = vecNormalized(lookfrom - lookat);     // w 与看向的方向反向
+        Vec3d u = vecNormalized(crossProduct(viewup, w));
+        Vec3d v = crossProduct(w, u);
 
-        origin_ = Point3d({0, 0, 0});
-        horizontal_ = Vec3d({viewport_width, 0.0, 0.0});
-        vertical_ = Vec3d({0.0, viewport_height, 0.0});
+        origin_ = lookfrom;
+        horizontal_ = viewport_width * u;   // viewport 水平方向的单位向量
+        vertical_ = viewport_height * v;    // viewport 垂直方向的单位向量
         // 视口左下角的坐标
-        lower_left_corner_ = origin_ - horizontal_ / 2.0 - vertical_ / 2.0 - Vec3d({0, 0, focal_length});
+        lower_left_corner_ = origin_ - horizontal_ / 2.0 - vertical_ / 2.0 - w;     // 因为 focal_length == 1.0，所以 w 没有修饰
     }
 
+    // 这里传入的 u, v 都是 0-1 之间的小数
     Ray getRay(double u, double v) const {
         return Ray(origin_, lower_left_corner_ + u * horizontal_ + v * vertical_ - origin_);
     }
