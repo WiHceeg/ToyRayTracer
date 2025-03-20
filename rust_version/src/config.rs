819af47fbc1835bf0197db22e518949ad923d656
WiHceeg <835235763@qq.com>
2025-03-21 05:08:32 +0800

    RayTracingInOneWeekend6.1

--- rust_version/src/config.rs
+++ rust_version/src/config.rs
@@ -1,6 +1,6 @@
 use crate::color::Color;
 
-pub const IMAGE_WIDTH: usize = 256;
+pub const IMAGE_WIDTH: usize = 400;
 pub const ASPECT_RATIO: f64 = 16.0/9.0;
 
 // pub const IMAGE_HEIGHT: usize = 256;
