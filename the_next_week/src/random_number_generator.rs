ee5a9533ea37c81e91429ec3c0cb0ac6ec694285
WiHceeg <835235763@qq.com>
2025-04-05 03:58:00 +0800

    ThreadRng已经是线程本地存储的，内部通过thread_local!+RefCell实现，直接用就行，没必要重新封装

--- the_next_week/src/random_number_generator.rs
+++ the_next_week/src/random_number_generator.rs
@@ -1,21 +1,16 @@
-use rand::rngs::ThreadRng;
-use rand::Rng;
-use std::cell::RefCell;
 use std::ops::Range;
 
-thread_local! {
-    pub static RNG: RefCell<ThreadRng> = RefCell::new(rand::rng());
-}
+use rand::rngs::ThreadRng;
 
 pub fn get_random_generator() -> ThreadRng {
-    RNG.with(|rng| rng.borrow_mut().clone())
+    rand::rng()
 }
 
 /// Generates a random number in the range [0, 1)
 pub fn random() -> f64{
-    RNG.with(|rng| rng.borrow_mut().random())
+    rand::random()
 }
 
 pub fn random_range(range: Range<f64>) -> f64 {
-    RNG.with(|rng| rng.borrow_mut().random_range(range))
+    rand::random_range(range)
 }
