062724a5224e73e1f31fe6d90decfe7d1bdbf92f
WiHceeg <835235763@qq.com>
2025-04-06 01:11:43 +0800

    RayTracingTheRestOfYourLife2.2

--- the_rest_of_your_life/src/pi.rs
+++ the_rest_of_your_life/src/pi.rs
@@ -3,15 +3,19 @@ mod random_number_generator;
 use random_number_generator::random_range;
 
 fn main() {
-    let N = 100000000;
-    let mut inside_circle = 0;
-    for _ in 0..N {
+    let mut inside_circle: u64 = 0;
+    let mut runs: u64 = 0;
+    loop {
+        runs += 1;
         let x = random_range(-1.0..1.0);
         let y = random_range(-1.0..1.0);
         if x * x + y * y < 1.0 {
             inside_circle += 1;
         }
+        if runs % 1000000 == 0 {
+            let pi = 4.0 * inside_circle as f64 / runs as f64;
+            print!("\rEstimated of Pi after {} runs: {:.12}", runs, pi);
+        }
     }
-    let pi = 4.0 * inside_circle as f64 / N as f64;
-    println!("Estimated value of pi: {:.12}", pi);
+
 }
